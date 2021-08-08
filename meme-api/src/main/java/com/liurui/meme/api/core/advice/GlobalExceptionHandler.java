b81f01fc2728477884f606ffe13b0781c09cd4c8
About-Gleaning <1851054389@qq.com>
2021-08-08 21:02:49 +0800

    feature: 异常

--- meme-api/src/main/java/com/liurui/meme/api/core/advice/GlobalExceptionHandler.java
+++ meme-api/src/main/java/com/liurui/meme/api/core/advice/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.liurui.meme.api.core.advice;
 
 import com.liurui.arsenal.base.web.bean.enums.ErrorCodeMsg;
 import com.liurui.arsenal.base.web.bean.result.BaseResult;
+import com.liurui.arsenal.base.web.exception.api.ApiException;
+import com.liurui.arsenal.base.web.exception.business.BusinessException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,6 +21,33 @@ public class GlobalExceptionHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
+    /**
+     * api异常
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(ApiException.class)
+    public BaseResult apiException(Exception e) {
+        LOGGER.error("apiException error", e);
+        return BaseResult.fail(ErrorCodeMsg.API_ERROR);
+    }
+
+    /**
+     * 业务异常
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(BusinessException.class)
+    public BaseResult businessException(Exception e) {
+        LOGGER.error("businessException error", e);
+        return BaseResult.fail(ErrorCodeMsg.BUSINESS_ERROR);
+    }
+
+    /**
+     * 未知异常
+     * @param e
+     * @return
+     */
     @ExceptionHandler(Exception.class)
     public BaseResult exception(Exception e) {
         LOGGER.error("application error", e);
