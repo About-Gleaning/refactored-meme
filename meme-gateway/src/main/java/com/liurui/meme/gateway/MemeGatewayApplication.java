ba7e74a7874e74d544ba89ec028364729e17f5fd
About-Gleaning <1851054389@qq.com>
2021-08-12 16:15:00 +0800

    feature: 新增gateway模块

---
+++ meme-gateway/src/main/java/com/liurui/meme/gateway/MemeGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.liurui.meme.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemeGatewayApplication.class, args);
+    }
+
+}
